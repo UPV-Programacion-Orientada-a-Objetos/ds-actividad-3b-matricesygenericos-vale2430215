1baf9900f1e3e25300aa8c505a3b903dc01d690b
Valeria Bermudez <2430215@upv.edu.mx>
2025-10-14 16:48:22 -0600

    ingresar datos a la matriz

--- MatrizDinamica.h
+++ MatrizDinamica.h
@@ -1,3 +1,6 @@
+#include <iostream>
+using namespace std;
+
 template <typename T>
 class MatrizDinamica {
 private:
@@ -6,5 +9,34 @@ private:
     int columnas;
 
 public:
-    // Constructor, Destructor, Métodos de gestión y operaciones
+    // Constructor 
+     MatrizDinamica(int f, int c) {
+        filas = f;
+        columnas = c;
+
+        datos = new T*[filas];
+        for (int i = 0; i < filas; i++)
+            datos[i] = new T[columnas];
+    }
+
+
+    //Destructor
+    ~MatrizDinamica() {
+        for (int i = 0; i < filas; i++)
+            delete[] datos[i];
+        delete[] datos;
+    }
+
+    //Ingresar
+    void ingresar() {
+        cout << "Ingresa los valores de la matriz (" << filas << "x" << columnas << "):\n";
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                cout << "[" << i << "][" << j << "]: ";
+                cin >> datos[i][j];
+            }
+        }
+    }
+
+    //Métodos de gestión y operaciones
 };
--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 #include "MatrizDinamica.h"
 
+using namespace std;
+
 int main() {
+    MatrizDinamica<int> A(2,3);
+    MatrizDinamica<int> B(3,2);
+
+    cout<<"Matriz A: \n";
+    A.ingresar();
+
+    cout<<"Matriz B: \n";
+    B.ingresar();
+    
     return 0;
 }
