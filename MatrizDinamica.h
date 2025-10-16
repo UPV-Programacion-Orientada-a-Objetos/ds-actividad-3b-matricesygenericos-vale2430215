26655880d5cf5827c6652b0dca109f81dd94c180
Valeria Bermudez <2430215@upv.edu.mx>
2025-10-15 20:16:13 -0600

    matriz float

--- MatrizDinamica.h
+++ MatrizDinamica.h
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <stdexcept> //para manejar errores
+
 using namespace std;
 
 template <typename T>
@@ -15,8 +17,11 @@ public:
         columnas = c;
 
         datos = new T*[filas];
-        for (int i = 0; i < filas; i++)
+        for (int i = 0; i < filas; i++){
             datos[i] = new T[columnas];
+            for (int j = 0; j < columnas; j++)
+                datos[i][j] = 0;
+        }
     }
 
 
@@ -39,7 +44,7 @@ public:
     }
 
     //Mostrar matriz
-    void mostrar() {
+    void mostrar() const{
         for (int i = 0; i < filas; i++) {
             for (int j = 0; j < columnas; j++)
                 cout << datos[i][j] << "\t";
@@ -67,4 +72,54 @@ public:
 
         return C;
     }
+
+    //Redimensionar la matriz
+    void redimensionar(int nuevaF, int nuevaC) {
+        // Crear nueva matriz
+        T **nueva = new T*[nuevaF];
+        for (int i = 0; i < nuevaF; i++){
+            nueva[i] = new T[nuevaC];
+            for (int j = 0; j < nuevaC; j++)
+                nueva[i][j] = 0; // inicializa en 0
+        }
+
+        //Copiar los valores
+        int minFilas = (nuevaF < filas) ? nuevaF : filas;
+        int minCols = (nuevaC < columnas) ? nuevaC : columnas;
+
+        for (int i = 0; i < minFilas; i++)
+            for (int j = 0; j < minCols; j++)
+                nueva[i][j] = datos[i][j];
+
+        //Liberar la antigua
+        for (int i = 0; i < filas; i++)
+            delete[] datos[i];
+        delete[] datos;
+
+        //Actualizar
+        datos = nueva;
+        filas = nuevaF;
+        columnas = nuevaC;
+    }
+
+    // Multiplicación genérica
+    static MatrizDinamica<T> multiplicar(const MatrizDinamica<T>& A, const MatrizDinamica<T>& B) {
+        if (A.columnas != B.filas)
+            throw invalid_argument("Error: no se pueden multiplicar (columnas de A != filas de B)");
+
+        MatrizDinamica<T> C(A.filas, B.columnas);
+
+        for (int i = 0; i < A.filas; i++) {
+            for (int j = 0; j < B.columnas; j++) {
+                C.datos[i][j] = 0;
+                for (int k = 0; k < A.columnas; k++)
+                    C.datos[i][j] += A.datos[i][k] * B.datos[k][j];
+            }
+        }
+
+        return C;
+    }
+
+
+
 };
--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 int main() {
+    cout << "MATRIZ INT\n";
     MatrizDinamica<int> A(2,3);
     MatrizDinamica<int> B(3,2);
 
@@ -16,6 +17,25 @@ int main() {
     cout<<"Multiplicacion de A x B\n";
     MatrizDinamica<int> C = MatrizDinamica<int>::multiplicar(A, B);
     C.mostrar();
+
+    cout << "\nRedimensionando la matriz C a (3x3)...\n";
+    C.redimensionar(3, 3);
+    C.mostrar();
+
+    cout << "\nMATRIZ FLOAT\n";
+    MatrizDinamica<float> X(2, 2);
+    MatrizDinamica<float> Y(2, 2);
+
+    cout<<"Matriz X: \n";
+    X.ingresar();
+    cout<<"Matriz Y: \n";
+    Y.ingresar();
+
+    cout<<"Multiplicacion de X x Y\n";
+    MatrizDinamica<float> Z = MatrizDinamica<float>::multiplicar(X, Y);
+    Z.mostrar();
+
+    cout<<"Memoria liberada\n";
     
     return 0;
 }
